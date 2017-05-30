46d90cac44130a5852d7d6257369db2569360565
lemonaha <641301711@qq.com>
2017-05-30 16:45:57 +0800

    change the exe file of test

--- test.cpp
+++ test.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
+int addNewFunction(int x,int y)
+{
+    std::cout<< "add a new function"<<std::endl;
+    return x+y;
+}
+
+
 int main()
 {
     std::cout << "hello lemon,its a test cpp" << std::endl;
+    addNewFunction(2,3); 
     return 0;
 }
 
-int addNewFunciton(int x,int y)
-{
-    return x+y;
-}
